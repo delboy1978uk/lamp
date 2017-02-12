cab987df306c5f924c19bb4f9e4f48f63a098f24
delboy1978uk <delboy1978uk@gmail.com>
2017-02-12 18:58:44 +0100

    adding 'app' for mounting data

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
-<h1>Hello, it is <?= date('d/m/Y'); ?></h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>delboy1978uk/devbox</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <img src="img/skull_and_crossbones.png"/>
+        </div>
+      </div>
+    <h1 style="text-align: center;"><br />Hello, it is <?= date('d/m/Y H:i'); ?></h1>
+    <br class="clearfix">
+    <?= phpinfo(); ?>
+  </div>
+  </body>
+</html>
